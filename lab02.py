15eb009b6cbe9e81dcbe1fb0df608160fa69fed5
mTy8421 <koontyza007@gmail.com>
2023-11-14 18:21:10 +0700

    update

--- lab02.py
+++ lab02.py
@@ -8,37 +8,53 @@ obj = json.load(
 )
 length = len(obj["stations"][0]["data"])
 
-print("Station ID: " + obj["stations"][0]["stationID"])
-
 while True:
-    text = input(str("Enter Someting: "))
+    text = input(
+        str(
+            "Type [PM25, PM10, O3, CO, NO2, SO2, WS, WD, TEMP, RH, BP, RAIN] for Show or Type [EXIT] for End : "
+        )
+    )
     txt = text.upper()
 
     match txt:
         case "PM25":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("PM25")
         case "PM10":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("PM10")
         case "O3":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("O3")
         case "CO":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("CO")
         case "NO2":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("NO2")
         case "SO2":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("SO2")
         case "WS":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("WS")
         case "WD":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("WD")
         case "TEMP":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("TEMP")
         case "RH":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("RH")
         case "BP":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("BP")
         case "RAIN":
+            print("Station ID: " + obj["stations"][0]["stationID"])
             print("RAIN")
+        case "EXIT":
+            break
         case _:
             for k, v in obj["stations"][0]["data"][0].items():
                 print(k, end=" ")
