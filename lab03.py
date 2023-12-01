bfbb07e0d3ce9811c57a7321ef470522a56d73cb
Panthakit totid <koontyza007@gmail.com>
2023-12-01 19:42:27 +0700

    update

--- lab03.py
+++ lab03.py
@@ -3,3 +3,14 @@ class PuzzleState:
         self.puzzle = puzzle
         self.size = len(puzzle)
         self.blk = puzzle.index(0)
+    
+    def __str__(self):
+        txt = ''
+
+        for i in range(0, self.size, 3):
+            for j in range(3):
+                if j > 0:
+                    txt += ''
+                txt += str(self.puzzle[j+i])
+            txt += '\n'
+        return txt
