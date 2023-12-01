704af49d8cae33187d1560eee45b278409062f54
Panthakit totid <koontyza007@gmail.com>
2023-12-01 19:48:30 +0700

    Update lab03.py

--- lab03.py
+++ lab03.py
@@ -14,3 +14,24 @@ class PuzzleState:
                 txt += str(self.puzzle[j+i])
             txt += '\n'
         return txt
+    
+    def move(self, direction):
+        new_state = PuzzleState(list(self.puzzle))
+        if direction == 'up' and new_state.blk >= 3:
+            new_state.puzzle[new_state.blk], new_state.puzzle[new_state.blk - 3] = (
+                new_state.puzzle[new_state.blk - 3],
+                new_state.puzzle[new_state.blk],
+            )
+            new_state.blk -= 3
+        elif direction == 'down' and new_state.blk > 6:
+            new_state.puzzle[new_state.blk], new_state.puzzle[new_state.blk + 3] = (
+                new_state.puzzle[new_state.blk + 3],
+                new_state.puzzle[new_state.blk],
+            )
+            new_state.blk += 3
+        elif direction == 'left' and new_state.blk % 3 != 0:
+            new_state.puzzle[new_state.blk], new_state.puzzle[new_state.blk - 1] = (
+                new_state.puzzle[new_state.blk - 1],
+                new_state.puzzle[new_state.blk],
+            )
+            new_state.blk -= 1
